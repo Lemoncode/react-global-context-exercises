d4887ee325e1b1514967291e6b1768c82deebdca
nasdan <idsm00012@gmail.com>
2019-09-24 21:17:21 +0200

    copy previous example

---
+++ 02-split-context/readme.md
@@ -0,0 +1,29 @@
+# 02 Split context
+
+In this example we will create a global state to manage app state (login, hotels, edit hotel, etc.)
+
+## Prerequisites
+
+Install [Node.js and npm](https://nodejs.org/en/) if they are not already installed on your computer.
+
+## Steps to build it
+
+- Copy the content of the `00-boiler` folder to an empty folder for the sample.
+
+- Install the npm packages described in the [./package.json](./package.json) and verify that it works:
+
+```bash
+npm install
+```
+
+-
+
+# About Basefactor + Lemoncode
+
+We are an innovating team of Javascript experts, passionate about turning your ideas into robust products.
+
+[Basefactor, consultancy by Lemoncode](http://www.basefactor.com) provides consultancy and coaching services.
+
+[Lemoncode](http://lemoncode.net/services/en/#en-home) provides training services.
+
+For the LATAM/Spanish audience we are running an Online Front End Master degree, more info: http://lemoncode.net/master-frontend
---
+++ 02-split-context/src/pods/hotel-edit/hotel-edit.container.jsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+import { GlobalStateContext } from 'core/context';
+import { history, linkRoutes } from 'core/router';
+import { createEmptyHotel, createEmptyHotelErrors } from './hotel-edit.vm';
+import { formValidation } from './hotel-edit.validation';
+import { getCities } from './api';
+import { HotelEditComponent } from './hotel-edit.component';
+
+const InnerHotelEditContainer = props => {
+  const { match } = props;
+  const { state, dispatch } = React.useContext(GlobalStateContext);
+  const [hotel, setHotel] = React.useState(createEmptyHotel());
+  const [hotelErrors, setHotelErrors] = React.useState(
+    createEmptyHotelErrors()
+  );
+  const [cities, setCities] = React.useState([]);
+
+  React.useEffect(() => {
+    getCities().then(setCities);
+  }, []);
+
+  React.useEffect(() => {
+    const selectedHotel = state.hotelCollection.find(
+      h => h.id === match.params.id
+    );
+    setHotel(selectedHotel ? selectedHotel : createEmptyHotel());
+  }, [match.params.id]);
+
+  const onFieldUpdate = (fieldId, value) => {
+    setHotel({
+      ...hotel,
+      [fieldId]: value,
+    });
+
+    formValidation.validateField(fieldId, value).then(validationResult => {
+      setHotelErrors({
+        ...hotelErrors,
+        [fieldId]: validationResult,
+      });
+    });
+  };
+
+  const handleSave = () => {
+    formValidation.validateForm(hotel).then(formValidationResult => {
+      if (formValidationResult.succeeded) {
+        const hotelCollection = state.hotelCollection.map(h =>
+          h.id === hotel.id
+            ? {
+                ...hotel,
+              }
+            : h
+        );
+        dispatch({ hotelCollection });
+        history.push(linkRoutes.hotelCollection);
+      } else {
+        setHotelErrors(formValidationResult.fieldErrors);
+      }
+    });
+  };
+
+  return (
+    <HotelEditComponent
+      hotel={hotel}
+      cities={cities}
+      onFieldUpdate={onFieldUpdate}
+      hotelErrors={hotelErrors}
+      onSave={handleSave}
+    />
+  );
+};
+
+export const HotelEditContainer = withRouter(InnerHotelEditContainer);
